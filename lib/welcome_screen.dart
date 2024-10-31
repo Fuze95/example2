cca51dd11b38c32e8ee9894d5d44719562bab07f
Fuze95 <60931780+Fuze95@users.noreply.github.com>
2024-10-31 13:04:04 +0530

    Welcome Screen 2

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Image.asset('assets/SS.jpg'),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text('Welcome!',
+                style: TextStyle(
+                  fontSize: 42.0,
+                  fontFamily: 'Lora',
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text('Join with us & Enjoy Healthy Life!',
+                style: TextStyle(
+                  fontSize: 14.0
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[600],
+                  shape: BeveledRectangleBorder(
+                  borderRadius : BorderRadius.circular(2),
+                  ),
+                ),
+                child: const Text(
+                  "OK",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
